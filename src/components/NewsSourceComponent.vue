a66e9f9f6729fdd5af60de988449de1c535f73b4
Azjargal13 <azjargal.gankhuyag@konicaminolta.com>
2019-12-13 10:44:34 +0900

    initial project setting ups

--- src/components/NewsSourceComponent.vue
+++ src/components/NewsSourceComponent.vue
@@ -1,27 +1,22 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="450"
-  >
+  <v-card class="mx-auto" max-width="450">
     <v-img
       src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
-      :aspect-ratio="16/9"
+      :aspect-ratio="16 / 9"
     >
-
     </v-img>
     <v-card-title>
       <div class="display-1 mb-2">Welcome Home...</div>
-      <div class="title font-weight-regular grey--text">Monday, 12:30 PM, Mostly Sunny</div>
+      <div class="title font-weight-regular grey--text">
+        Monday, 12:30 PM, Mostly Sunny
+      </div>
     </v-card-title>
-    <v-row
-      class="px-4 grey--text"
-      align="center"
-    >
-      <v-avatar
-        size="24"
-        class="mr-4"
-      >
-        <v-img src="https://cdn.vuetifyjs.com/images/weather/part-cloud-48px.png" contain></v-img>
+    <v-row class="px-4 grey--text" align="center">
+      <v-avatar size="24" class="mr-4">
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/weather/part-cloud-48px.png"
+          contain
+        ></v-img>
       </v-avatar>
 
       <span>81° / 62°</span>
@@ -30,17 +25,11 @@
     <v-divider class="mt-6 mx-4"></v-divider>
 
     <v-card-text>
-      <v-chip
-        class="mr-2"
-        @click="lights"
-      >
+      <v-chip class="mr-2" @click="lights">
         <v-icon left>mdi-brightness-5</v-icon>
         Turn on Lights
       </v-chip>
-      <v-chip
-        class="mr-2"
-        @click="alarm"
-      >
+      <v-chip class="mr-2" @click="alarm">
         <v-icon left>mdi-alarm-check</v-icon>
         Set alarm
       </v-chip>
@@ -53,7 +42,7 @@
 </template>
 
 <script lang="ts">
-export default{
-  name:"NewsSourceComponent"
-}
+export default {
+  name: "NewsSourceComponent"
+};
 </script>
