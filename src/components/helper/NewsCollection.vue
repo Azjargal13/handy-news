1ac317ab28aeecd08fd1cb910685a4b3fb6cdb3c
Azjargal13 <azjargal.gankhuyag@konicaminolta.com>
2019-12-17 14:01:05 +0900

    news card layout

--- src/components/helper/NewsCollection.vue
+++ src/components/helper/NewsCollection.vue
@@ -1,28 +1,27 @@
 <template>
-  <div class="d-flex">
+  <div class="d-md-flex flex-wrap">
     <!-- in cicle logo shows news source -->
     <v-card
-      class="mx-5 mt-5"
+      class="mx-5 my-5"
       max-width="344"
       style="text-align:left"
       v-for="news in newsAll"
       :key="news.id"
     >
       <v-list-item three-line>
+        <v-list-item-avatar size="50" color="grey"></v-list-item-avatar>
         <v-list-item-content>
-          <div class="overline mb-4">OVERLINE</div>
+          <div class="overline mb-4">News from {{ news.publisher }}</div>
           <v-list-item-title class="headline mb-1"
-            >Headline 5</v-list-item-title
-          >
-          <v-list-item-subtitle
-            >Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle
-          >
+            >{{ news.title }}
+          </v-list-item-title>
+
+          <v-list-item-subtitle> {{ news.content }} </v-list-item-subtitle>
         </v-list-item-content>
-        <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
       </v-list-item>
       <v-card-actions>
-        <v-btn text>Button</v-btn>
-        <v-btn text>Button</v-btn>
+        <v-btn text>Hide</v-btn>
+        <v-btn text>Read more</v-btn>
       </v-card-actions>
     </v-card>
   </div>
@@ -37,10 +36,12 @@ interface newsBundle {
   content: string;
   publisher: string;
   publishedOn: string;
+  category: string;
 }
 
 @Component({})
 export default class NewsCollection extends Vue {
+  private hideNewsCard: boolean = false;
   constructor() {
     super();
   }
@@ -49,25 +50,36 @@ export default class NewsCollection extends Vue {
       title: "title1",
       content: "content1",
       publisher: "publisher1",
-      publishedOn: "17-Dec, 19"
+      publishedOn: "17-Dec, 19",
+      category: "sport"
     },
     {
       title: "title2",
       content: "content2",
       publisher: "publisher2",
-      publishedOn: "17-Dec, 19"
+      publishedOn: "17-Dec, 19",
+      category: "business"
     },
     {
       title: "title3",
       content: "content3",
       publisher: "publisher3",
-      publishedOn: "17-Dec, 19"
+      publishedOn: "17-Dec, 19",
+      category: "market"
     },
     {
       title: "title4",
       content: "content4",
-      publisher: "publisher",
-      publishedOn: "17-Dec, 19"
+      publisher: "publisher4",
+      publishedOn: "17-Dec, 19",
+      category: "technology"
+    },
+    {
+      title: "title5",
+      content: "content5",
+      publisher: "publisher5",
+      publishedOn: "17-Dec, 19",
+      category: "market"
     }
   ];
 
