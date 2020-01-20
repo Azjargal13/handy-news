3729e4a5f8d6940661635df92b9936e8a50924ab
Azjargal13 <azjargal.gankhuyag@konicaminolta.com>
2020-01-20 13:46:28 +0900

    sample docs for storybook

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Hello from "../components/helper/Hello.vue";
 
-export default { title: "Footer component" };
+export default { title: "Footer component", component: Hello };
 
 export const withText = () => "<hello>yupp, normal test</hello>";
 export const asAComponent = () => ({
